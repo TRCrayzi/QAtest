8d771381f7ee1fe94ee01baada38675aeaa5a651
TRCrayzi <46112432+TRCrayzi@users.noreply.github.com>
2023-04-10 17:50:51 +0300

    Test Case 1, Test Case 2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,53 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\main\\drivers\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String baseUrl = System.getProperty("user.dir") + "\\src\\main\\resources\\AQA_Test_Task.html";
+
+        driver.get(baseUrl);
+        WebElement selectModel = driver.findElement(By.xpath("//*[@id='model1']/p[2]/select"));
+        String[] Models = selectModel.findElements(By.tagName("option"))
+                .stream()
+                .map(WebElement::getText)
+                .toArray(String[]::new);
+
+        WebElement selectColor = driver.findElement(By.xpath("//*[@id=\"vehColor\"]"));
+        String[] Colors = selectColor.findElements(By.tagName("option"))
+                .stream()
+                .map(WebElement::getText)
+                .toArray(String[]::new);
+
+        TestPage testpage =new TestPage(driver);
+
+        //Task 1
+        //test
+        Thread.sleep(500);
+        testpage.TestInput("Vehicle 1", Models[1], Colors[1], "2020");
+
+        Thread.sleep(5000);
+        driver.navigate().refresh();
+
+        //Task 2
+        //test
+        Thread.sleep(500);
+        testpage.TestModification("Vehicle 1", Models, Colors[1], "2020");
+
+        Thread.sleep(10000);
+        driver.quit();
+
     }
 }
--- src/main/java/org/example/TestPage.java
+++ src/main/java/org/example/TestPage.java
@@ -1,2 +1,74 @@
-package org.example;public class TestPage {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPage {
+    private WebDriver driver;
+
+    private final By CName = By.xpath("//input[@class=\"js-name\"]");
+    private final By CModel = By.xpath("//*[@id='model1']/p[2]/select");
+    private final By CYear = By.xpath("//input[@class=\"js-year\"]");
+    private final By CColor = By.xpath("//*[@id=\"vehColor\"]");
+    private final By CClone = By.xpath("//*[@id=\"cloneVehicle\"]");
+
+    public TestPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void EnterCName(String Name) {
+        driver.findElement(CName).sendKeys(Name, Keys.ENTER);
+    }
+
+    public void ChooseCModel(String Model) {
+        driver.findElement(CModel).click();
+        driver.findElement(By.xpath("//*[@value=\"" + Model + "\"]")).click();
+    }
+
+    public void EnterCYear(String Year) {
+        driver.findElement(CYear).sendKeys(Year, Keys.ENTER);
+    }
+
+    public void ChooseCColor(String Color) {
+        driver.findElement(CColor).click();
+        driver.findElement(By.xpath("//*[@value=\"" + Color + "\"]")).click();
+    }
+
+    public void ClickCClone() {
+        driver.findElement(CClone).click();
+    }
+
+
+    @Test
+    public void TestInput(String Name, String Model, String Color, String Year) {
+        EnterCName(Name);
+        ChooseCModel(Model);
+        EnterCYear(Year);
+        ChooseCColor(Color);
+        ClickCClone();
+        Asserts( Name, Model, Color, Year);
+    }
+
+    @Test
+    public void TestModification(String Name, String[] Models, String Color, String Year) {
+        EnterCName(Name);
+        ChooseCModel(Models[1]);
+        EnterCYear(Year);
+        //ClickCClone();
+        //Thread.sleep(2000);
+        ChooseCModel(Models[2]);
+        ChooseCColor(Color);
+        ClickCClone();
+        Asserts( Name, Models[2], Color, Year);
+    }
+
+    public void Asserts(String Name, String Model, String Color, String Year) {
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"model2\"]/p[1]/input")).getAttribute("value"), Name );
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"model2\"]/p[2]/select")).getAttribute("value"),Model );
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"model2\"]/p[3]/input")).getAttribute("value"),Color );
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"model2\"]/p[4]/input")).getAttribute("value"),Year );
+    }
 }
